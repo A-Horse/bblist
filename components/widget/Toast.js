16384522e33d0529abbe37f1e6fb32576cef03e7
fwchen <me@chenfangwei.com>
2016-08-07 23:16:11 +0800

    initial commit

---
+++ components/widget/Toast.js
@@ -0,0 +1,97 @@
+import React, { Component, PropTypes } from 'react';
+
+import {Modal} from './Modal';
+import R from 'fw-ramda';
+import {getAssets} from '../../services/assets-manager';
+
+const modalStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(20, 20, 20, 0.75)'
+  },
+  content: {
+    position: 'absolute',
+    top: '140px',
+    left: '140px',
+    right: '140px',
+    bottom: '140px',
+    border: '1px solid #ccc',
+    background: '#fff',
+    overflow: 'auto',
+    WebkitOverflowScrolling: 'touch',
+    borderRadius: '4px',
+    outline: 'none',
+    padding: '20px'
+  }
+};
+
+const styles = {
+  main: {
+    
+  },
+  close: {
+    position: 'absolute',
+    
+  }
+};
+
+export class Toast extends Component {
+  propTypes: {
+    confirmFn: PropTypes.func.isRequired
+  }
+  
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    }
+  }
+  
+  init() {
+    
+  }
+
+  componentDidMount() {
+    
+  }
+
+  componentDidUpdate() {
+    
+  }
+
+  cancel() {
+    this.props.cancelFn && this.props.cancelFn();
+    this.close();
+  }
+
+  confirm() {
+    this.props.confirmFn();
+    this.close();
+  }
+
+  close() {
+    this.setState({isOpen: false})
+  }
+
+  open() {
+    this.setState({isOpen: true})
+  }
+  
+  render() {
+    return (
+      <Modal styles={modalStyles} isOpen={this.state.isOpen}>
+        <div onClick={() => {this.close()}}>
+          <img src={getAssets('svg', 'clear')} />
+        </div>
+        <div>
+          <button onClick={() => {this.confirm()}}>Confirm</button>
+          <button onClick={() => {this.cancel()}}>Cancel</button>
+        </div>
+      </Modal>
+    );
+  }
+}
