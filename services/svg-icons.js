42fd6b6def60adb98a565db9e3f0f665f99ccbbc
fwchen <me@chenfangwei.com>
2016-08-29 11:45:17 +0800

    change svg path

--- services/svg-icons.js
+++ services/svg-icons.js
@@ -1,5 +1,6 @@
-export CloseIcon from 'babel!svg-react!../static/svg/ic_clear_black_24px.svg?name=CloseIcon';
-export LightIcon from 'babel!svg-react!../static/svg/ic_lightbulb_outline_black_24px.svg?name=LightIcon';
-export AddIcon from 'babel!svg-react!../static/svg/ic_add_black_24px.svg?name=AddIcon';
-export EditIcon from 'babel!svg-react!../static/svg/ic_mode_edit_black_24px.svg?name=EditIcon';
-export ArrowDownIcon from 'babel!svg-react!../static/svg/ic_keyboard_arrow_down_black_24px.svg?name=ArrowDownIcon';
+export CloseIcon from 'babel!svg-react!../assets/svg/ic_clear_black_24px.svg?name=CloseIcon';
+export LightIcon from 'babel!svg-react!../assets/svg/ic_lightbulb_outline_black_24px.svg?name=LightIcon';
+export AddIcon from 'babel!svg-react!../assets/svg/ic_add_black_24px.svg?name=AddIcon';
+export EditIcon from 'babel!svg-react!../assets/svg/ic_mode_edit_black_24px.svg?name=EditIcon';
+export ArrowDownIcon from 'babel!svg-react!../assets/svg/ic_keyboard_arrow_down_black_24px.svg?name=ArrowDownIcon';
+
