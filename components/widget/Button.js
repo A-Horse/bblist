d8e7aa682544dd67c28452a21ccaf128195c4130
fwchen <me@chenfangwei.com>
2016-09-05 16:37:02 +0800

    add Button widget

---
+++ components/widget/Button.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {ThemeConst} from '../../style/theme';
+
+const styles = {
+  button: {
+    borderRadius: '3px'
+  },
+  small: {
+    
+  },
+  midde: {
+    
+  },
+  large: {
+    
+  },
+  default: {
+    border: `1px solid ${ThemeConst.border.color.default}`,
+    backgroundColor: '#fff'
+  },
+  primary: {
+    backgroundColor: ThemeConst.color.primary
+  },
+  disable: {
+    backgroundColor: ThemeConst.color.disable
+  }
+};
+
+export class Button extends Component {
+  static propTypes = {
+    size: React.PropTypes.oneOf(['small', 'middle', 'large']),
+    type: React.PropTypes.oneOf(['default', 'primary', 'disable'])
+  };
+  
+  render() {
+    const {style, size = 'middle', type = 'default'} = this.props;
+    return (
+      <button style={Object.assign(styles.button, style, styles[size], styles[type])}
+              onClick={this.props.onClick}>
+          {this.props.children}
+      </button>
+    );
+  }
+}
