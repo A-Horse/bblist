e46672e4923e47ffa16bb862d22d9083ec83e47d
fwchen <me@chenfangwei.com>
2016-09-05 16:42:50 +0800

    add semicolons

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createStore, combineReducers} from 'redux'
-import {Provider} from 'react-redux'
-import {Router, Route, IndexRoute, browserHistory} from 'react-router'
-import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
-import {applyMiddleware} from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import {createDevTools} from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
-import DockMonitor from 'redux-devtools-dock-monitor'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
+import {applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createDevTools} from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
 
-import {App, Login, SignUp, Tasks, TaskWall, NotFound, DashBoard,
-        Profile, IndexPage, Ideas, TaskWallSetting} from './components'
-import {checkLogin} from './utils/auth'
-import * as reducers from './reducers'
+import {App, Tasks, TaskWall, NotFound, DashBoard,
+        IndexPage, Ideas, TaskWallSetting} from './components';
+import {Login, SignUp, Profile, Goal} from './page';
+
+import {checkLogin} from './utils/auth';
+import * as reducers from './reducers';
 
 import './style/normalize.css';
 import './style/app.scss';
@@ -21,35 +23,37 @@ import './style/app.scss';
 const reducer = combineReducers({
   ...reducers,
   routing: routerReducer
-})
+});
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
     <LogMonitor theme="tomorrow" preserveScrollTop={false} />
   </DockMonitor>
-)
+);
 
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
 
 const store = createStoreWithMiddleware(
   reducer,
-  // DevTools.instrument()
-)
-const history = syncHistoryWithStore(browserHistory, store)
+   DevTools.instrument()
+);
+const history = syncHistoryWithStore(browserHistory, store);
 
-
+// TODO extract it router
 ReactDOM.render(
   <Provider store={store}>
     <div>
       <Router history={history}>
         <Route path="/" component={App} >
           <IndexRoute component={IndexPage}/>
+          
           <Route path="home" component={DashBoard} onEnter={checkLogin}/>
           <Route path="404" component={NotFound} />
           <Route path="login" component={Login}/>
           <Route path="signup" component={SignUp}/>
           <Route path="profile" component={Profile}/>
           <Route path="idea" component={Ideas}/>
+          <Route path="goal" component={Goal}/>
           <Route path="task-wall" component={Tasks} onEnter={checkLogin}/>
           <Route path="task-wall/:id" component={TaskWall} onEnter={checkLogin}/>
           <Route path="task-wall/:id/setting" component={TaskWallSetting} onEnter={checkLogin}/>
@@ -59,4 +63,4 @@ ReactDOM.render(
     </div>
   </Provider>,
   document.getElementById('root')
-)
+);
