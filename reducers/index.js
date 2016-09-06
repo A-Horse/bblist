778d5b99faf7026a54d095aa3bffafb41a354077
fwchen <me@chenfangwei.com>
2016-09-06 22:59:37 +0800

    refactor reducer index

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,8 @@
-export count from './count'
-export login from './login'
-export taskCard from './task-card'
-export signup from './sign-up'
-export taskWall from './task-wall'
-export taskList from './task-list'
+export login from './login';
+export signup from './sign-up';
+
+export taskCard from './task/task-card';
+export taskWall from './task/task-wall';
+export taskList from './task/task-list';
+
+export goal from './goal/goal';
