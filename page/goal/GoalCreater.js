a016446f2a5b647b6f74ae11a478528b45c2de7a
fwchen <me@chenfangwei.com>
2016-09-06 22:59:15 +0800

    create goal logic

--- page/goal/GoalCreater.js
+++ page/goal/GoalCreater.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {PageContainer} from '../../components/widget/PageContainer';
 import {connect} from 'react-redux';
 import {browserHistory} from 'react-router';
-import {makeGravatarUrl} from '../../services/gravatar';
+import {receiveGoalList, createGoal} from '../../actions/goal/goal';
+import {Button} from '../../components/widget/Button';
 
 const styles = {
   
@@ -18,15 +19,30 @@ class GoalCreater extends Component {
 
   render() {
     return (
+      <div>
         <div>
+          <input type='text' ref='goalTitle'/>
         </div>
+        <Button onClick={this.onClickCreateGoal.bind(this)}>Create Goal</Button>
+      </div>
     )
+  }
+
+  onClickCreateGoal() {
+    console.log('createGoal');
+    const {dispatch} = this.props;
+    const data = {
+      title: this.refs.goalTitle.value.trim()
+    };
+    dispatch(createGoal(data)).then(() => {
+      
+    });
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-
+    
   };
 };
 
