bef975cd1cb499c1aa3d769689bbd29f27a73493
fwchen <me@chenfangwei.com>
2016-09-08 22:12:33 +0800

    add Body component

---
+++ components/Body.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './input.scss';
+
+export class Body extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
