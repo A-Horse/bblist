dd6a47e4c831a9548cc6dd8d7ff589d1c75b61d0
fwchen <me@chenfangwei.com>
2016-09-09 22:32:27 +0800

    Body components

--- components/Body.js
+++ components/Body.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
-import './input.scss';
+import {connect} from 'react-redux';
 
-export class Body extends Component {
+const style = {
+  
+};
+
+class Body extends Component {
+
+  onClick(event) {
+    const {bubbleHandles} = this.props;
+    bubbleHandles.forEach(handle => handle(event));
+  }
+
+  onClickCapture(event) {
+    const {captureHandles} = this.props;
+    captureHandles.forEach(handle => handle(event));
+  }
   
   render() {
     return (
-      <div>
-        
+      <div onClick={this.onClick.bind(this)} onClickCapture={this.onClickCapture.bind(this)}>
+        {this.props.children}
       </div>
     );
   }
 }
+
+
+const mapStateToProps = (state) => {
+  return {
+    bubbleHandles: state.event.body.bubbleHandles,
+    captureHandles: state.event.body.captureHandles
+  }
+};
+
+export default connect(mapStateToProps)(Body);
