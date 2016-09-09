4a091d32e43bb08fb37a1ebc473f1934ab8d1eb1
fwchen <me@chenfangwei.com>
2016-09-09 22:32:36 +0800

    body event action update

--- actions/event/body.js
+++ actions/event/body.js
@@ -1,18 +1,38 @@
 export const BODY_ADD_EVENT = 'BODY_ADD_EVENT';
+export const BODY_REMOVE_EVENT = 'BODY_REMOVE_EVENT';
 export const BODY_ADD_EVENT_ONCE = 'BODY_ADD_EVENT_ONCE';
 
 function addBodyEvent(fn, useCapture) {
   return {
-    type: BODY_ADD_EVENT
+    type: BODY_ADD_EVENT,
+    fn,
+    useCapture
   };
 }
 
-function addBodyEventOnce() {
+function removeBodyEvent(fn) {
   return {
-    type: BODY_ADD_EVENT_ONCE
+    type: BODY_REMOVE_EVENT,
+    fn
   };
 }
 
-export function addBodyEventListener(fn, useCapture) {
-  
+function addBodyEventOnce(fn, useCapture) {
+  function onceFn() {
+    fn();
+    removeBodyEvent(fn);
+  }
+  return {
+    type: BODY_ADD_EVENT_ONCE,
+    fn: onceFn,
+    useCapture
+  };
+}
+
+export function addBodyEventListener(fn, useCapture = false) {
+  return dispatch => dispatch(addBodyEvent(fn, useCapture));
+}
+
+export function addBodyEventListenerOnce(fn, useCapture = false) {
+  return dispatch => dispatch(addBodyEventOnce(fn, useCapture));
 }
