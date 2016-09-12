733c4dce67ab559e8f5abb3691402851879fa2a2
fwchen <me@chenfangwei.com>
2016-09-12 17:58:07 +0800

    move index reference

--- components/index.js
+++ components/index.js
@@ -1,10 +1,2 @@
 export App from './App';
-export Tasks from './Tasks';
-export TaskWall from './TaskWall';
-export TaskWallSetting from './TaskWallSetting';
-export NotFound from './NotFound';
-export DashBoard from './DashBoard';
-export Setting from './Setting';
-export IndexPage from './IndexPage';
-export Ideas from './Ideas';
 
--- page/index.js
+++ page/index.js
@@ -1,4 +1,13 @@
-export Login from './Login';
+export SignIn from './SignIn';
 export SignUp from './SignUp';
 export Profile from './Profile';
 export Goal from './goal/Goal';
+export NotFound from './NotFound';
+export DashBoard from './DashBoard';
+export Setting from './Setting';
+export IndexPage from './IndexPage';
+export Idea from './idea/Idea';
+
+export Tasks from './task/Tasks';
+export TaskWall from './task/TaskWall';
+export TaskWallSetting from './task/TaskWallSetting';
