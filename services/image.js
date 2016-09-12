a53e86bd5b11ff038c59d12e514826fa51d966ba
fwchen <me@chenfangwei.com>
2016-09-12 22:30:16 +0800

    rename image serice function

--- services/image.js
+++ services/image.js
@@ -1,5 +1,5 @@
 
-export function getBase64Image(img) {
+export function getImageBase64(img) {
   let canvas = document.createElement('canvas');
   canvas.width = img.width;
   canvas.height = img.height;
