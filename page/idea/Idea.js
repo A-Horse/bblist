c240f850e013449b76fe834e55301362722029a3
fwchen <me@chenfangwei.com>
2016-09-12 17:59:24 +0800

    Idea page initial

---
+++ page/idea/Idea.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react';
+import fetch from 'isomorphic-fetch';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
+import {PageContainer} from '../../components/widget/PageContainer';
+
+class Ideas extends Component {
+  constructor() {
+    super()
+  }
+
+  getWalls() {
+    
+  }
+
+  componentWillMount() {
+
+  }
+  
+  render() {
+    return (
+      <PageContainer>
+        Ideas
+      </PageContainer>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps)(Ideas)
