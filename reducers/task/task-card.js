06454dac56d902f5e5e6c87684bea455ce6272f0
fwchen <me@chenfangwei.com>
2016-09-13 16:41:18 +0800

    add card patch reduce logic

--- reducers/task/task-card.js
+++ reducers/task/task-card.js
@@ -1,5 +1,6 @@
 import {
   TASKCARD_POST_REQUEST, TASKCARD_POST_SUCCESS, TASKCARD_POST_FAILURE,
+  TASKCARD_PATCH_REQUEST, TASKCARD_PATCH_SUCCESS, TASKCARD_PATCH_FAILURE,
 } from '../../actions/task/task-card';
 
 function taskCard(state = {
@@ -9,19 +10,35 @@ function taskCard(state = {
   case TASKCARD_POST_REQUEST:
     return Object.assign({}, state, {
       isFetching: true,
-      taskCard: action.taskCard
+      taskCard: action.card
     })
     break;
   case TASKCARD_POST_SUCCESS:
     return Object.assign({}, state, {
       isFetching: false,
-      taskCard: action.taskCard
+      taskCard: action.card
     })
     break;
   case TASKCARD_POST_FAILURE:
     return Object.assign({}, state, {
       isFetching: false,
       message: action.message
+    })
+    break;
+
+  case TASKCARD_PATCH_REQUEST:
+    return Object.assign({}, state, {
+      isFetching: true
+    })
+    break;
+  case TASKCARD_PATCH_SUCCESS:
+    return Object.assign({}, state, {
+      isFetching: false
+    })
+    break;
+  case TASKCARD_PATCH_FAILURE:
+    return Object.assign({}, state, {
+      isFetching: false
     })
     break;
 
