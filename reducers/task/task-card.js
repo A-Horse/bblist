650cbc04e64ff2a7c17b2c5aef8de950c20675e1
fwchen <me@chenfangwei.com>
2016-09-19 23:43:34 +0800

    add leave actions handle

--- reducers/task/task-card.js
+++ reducers/task/task-card.js
@@ -1,6 +1,9 @@
 import {
   TASKCARD_POST_REQUEST, TASKCARD_POST_SUCCESS, TASKCARD_POST_FAILURE,
   TASKCARD_PATCH_REQUEST, TASKCARD_PATCH_SUCCESS, TASKCARD_PATCH_FAILURE,
+  TASKCARD_LEAVE_START, TASKCARD_LEAVE_DONE,
+  TASKCARD_ENTER_START, TASKCARD_ENTER_DONE,
+  TASKCARD_MOVE_REQUEST, TASKCARD_MOVE_SUCCESS, TASKCARD_MOVE_FAILURE
 } from '../../actions/task/task-card';
 
 function taskCard(state = {
@@ -42,6 +45,12 @@ function taskCard(state = {
     })
     break;
 
+  case TASKCARD_LEAVE_START:
+  case TASKCARD_LEAVE_DONE:
+  case TASKCARD_ENTER_START:
+  case TASKCARD_ENTER_DONE:
+    return Object.assign({}, state);
+    
   default:
     return state;
   }
