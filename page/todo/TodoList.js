6e8d07f35902dbd5182fe298a22b544ea53c8611
fwchen <me@chenfangwei.com>
2016-09-20 22:55:38 +0800

    init todo

---
+++ page/todo/TodoList.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {PageContainer} from '../../components/widget/PageContainer';
+import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
+import {makeGravatarUrl} from '../../services/gravatar';
+import {getGoalList, createGoal} from '../../actions/goal/goal';
+import GoalCreater from './GoalCreater';
+import GoalCard from './GoalCard';
+
+const styles = {
+  
+};
+
+class TodoList extends Component {
+  componentWillMount() {
+    this.state = {
+      
+    };
+    this.getGoals();
+  }
+
+  componentDidUpdate() {
+    
+  }
+
+  render() {
+    return (
+      <PageContainer>
+        
+      </PageContainer>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    
+  };
+};
+
+export default connect(mapStateToProps)(TodoList);
