9d00786b2947249a36958d63013d682f0e64c79b
fwchen <me@chenfangwei.com>
2016-09-23 14:54:39 +0800

    flat todos direct to todo reducer

--- reducers/todo/todos.js
+++ reducers/todo/todos.js
@@ -3,20 +3,23 @@ import {
 } from 'actions/todo/todos';
 
 function todos(state = {
-  
+  isFetching: fasle
 }, action) {
   switch (action.type) {
   case TODOLIST_GET_REQUEST:
     return Object.assign({}, state, {
+      isFetching: true
     });
     break;
   case TODOLIST_GET_SUCCESS:
     return Object.assign({}, state, {
-      todos: action.todos
+      isFetching: false,
+      ...action.todos
     });
     break;
   case TODOLIST_GET_FAILURE:
     return Object.assign({}, state, {
+      isFetching: false,
       message: action.playload.message
     });
     break;
