b647658aca0b45544c69d3d68f940e6d7a48f49a
fwchen <me@chenfangwei.com>
2016-09-24 16:19:08 +0800

    remove conosle

--- utils/dom.js
+++ utils/dom.js
@@ -1,6 +1,5 @@
 
 export function getOffsetHeight(element, targetClassName, height = 0) {
-  console.log(element, targetClassName, height);
   if (~element.className.indexOf(targetClassName)) {
     return height;
   }
