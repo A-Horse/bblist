c1a5cd4de309bb262c3023a1194693d1c6415b69
fwchen <me@chenfangwei.com>
2016-09-26 11:01:30 +0800

    CardPlaceholder

---
+++ page/task/CardPlaceholder.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+import 'style/page/task/card-placeholder.scss';
+
+class CardPlaceholder extends Component {
+
+  buildStyle() {
+    const {height, width} = this.props;
+    return {
+      height: height + 'px',
+      width: width + 'px'
+    };
+  }
+  
+  render() {
+    return (
+      <div className='task-card-placeholder' style={this.buildStyle()}></div>
+    );
+  }
+}
+
+export default CardPlaceholder;
