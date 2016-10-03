58c321498cce4b80085c69ef92a7da969d1b40c2
fwchen <me@chenfangwei.com>
2016-10-03 20:57:23 +0800

    add icons

--- services/svg-icons.js
+++ services/svg-icons.js
@@ -5,6 +5,8 @@ export EditIcon from 'babel!svg-react!../assets/svg/ic_mode_edit_black_24px.svg?
 export ArrowDownIcon from 'babel!svg-react!../assets/svg/ic_keyboard_arrow_down_black_24px.svg?name=ArrowDownIcon';
 export SettingIcon from 'babel!svg-react!../assets/svg/ic_settings_black_24px.svg?name=SettingIcon';
 export DateIcon from 'babel!svg-react!../assets/svg/ic_date_range_black_24px.svg?name=DateIcon';
+export MoreIcon from 'babel!svg-react!../assets/svg/ic_more_horiz_black_24px.svg?name=MoreIcon';
+export AlarmIcon from 'babel!svg-react!../assets/svg/ic_alarm_black_24px.svg?name=AlarmIcon';
 
 export const SMALL_SIZE = 16;
 export const MIDDLE_SIZE = 20;
