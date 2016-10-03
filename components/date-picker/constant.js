cc2504ae99f401c6a36c785fe91588222197967f
fwchen <me@chenfangwei.com>
2016-10-03 19:11:10 +0800

    shortcut for week name

--- components/date-picker/constant.js
+++ components/date-picker/constant.js
@@ -1,5 +1,6 @@
 export const monthsName = [];
 
-export const weekDayName = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// export const weekDayName = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+export const weekDayName = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 export const weekFirstDay = 0;
