97b69405ee654f0a6825472780291034f315754b
fwchen <me@chenfangwei.com>
2016-10-07 14:43:34 +0800

    task

--- page/task/Tasks.js
+++ page/task/Tasks.js
@@ -10,6 +10,10 @@ import {CateLine} from 'components/widget/CateLine';
 import {spawnMixinRender} from 'style/theme-render';
 import {ThemeConst} from 'style/theme';
 import {CloseIcon, AddIcon} from 'services/svg-icons';
+import TaskBoardCreater from './TaskBoardCreater';
+
+import 'style/page/task/board.scss';
+import 'style/page/task/taskboard-creater-modal.scss';
 
 const styles = {
   wall: {
@@ -64,78 +68,6 @@ mainThemeRender('addIcon', 'deepDarkBackground');
 mainThemeRender('cardTitle', 'middleFontSize', 'lightText');
 mainThemeRender('createCardTitle', 'middleFontSize');
 
-const modalStyles = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(20, 20, 20, 0.75)'
-  },
-  content: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    width: '320px',
-    height: 'auto',
-    transform: 'translate(-50%, -60%)',
-    border: '1px solid #ccc',
-    background: '#fff',
-    WebkitOverflowScrolling: 'touch',
-    borderRadius: '4px',
-    outline: 'none',
-    padding: '20px'
-  },
-  closeButton: {
-    position: 'absolute',
-    width: '2rem',
-    height: '2rem',
-    top: '0.3rem',
-    right: '0.5rem',
-    borderRadius: '50%',
-    border: 'none',
-    backgroundColor: 'transparent'
-  },
-  clearIcon: {
-    verticalAlign: 'middle',
-    fill: '#999',
-    width: '1rem',
-    height: '1rem',
-    transition: 'all 80ms ease'
-  },
-  topbar: {
-    textAlign: 'center',
-    borderBottom: `1px solid ${ThemeConst.lightDark}`,
-    padding: '0 0 10px',
-    margin: '-8px 0 0'
-  },
-  image: {
-    maxWidth: '100%',
-    display: 'block',
-    margin: '0 auto'
-  },
-  quota: {
-    fontSize: ThemeConst.smallFontSize,
-    textAlign: 'center'
-  },
-  nameInput: {
-    width: '100%',
-    borderRadius: '1px',
-    border: `1px solid ${ThemeConst.deepDark}`,
-    margin: '10px 0',
-    padding: '4px 8px',
-    fontSize: ThemeConst.middleFontSize
-  },
-  createButton: {
-    width: '100%',
-    border: 'none',
-    fontSize: ThemeConst.middleFontSize
-  }
-};
-
-const createModalThemeRender = spawnMixinRender(modalStyles);
-createModalThemeRender('createButton', 'fullButton', 'mainColorBackground', 'lightText');
 
 @Radium
 class Tasks extends Component {
@@ -158,19 +90,7 @@ class Tasks extends Component {
   }
 
   renderCreateModal() {
-    return (
-      <Modal toggle={this.state.modalOpen} styles={modalStyles}>
-        <button key='modal-close' style={modalStyles.closeButton}>
-          <CloseIcon style={modalStyles.clearIcon} onClick={() => {this.setState({modalOpen: false})}}/>
-        </button>
-        
-        <div style={modalStyles.topbar}>Create Wall</div>
-        <img style={modalStyles.image} src='/static/image/d.png'/>
-        <p style={modalStyles.quota}>Establish their own projects for different transactions.</p>
-        <input style={modalStyles.nameInput} type='text' ref='name' placeholder="Wall Name"/>
-        <button style={modalStyles.createButton} onClick={() => this.handleClick()} >Complete And Create</button>
-      </Modal>
-    );
+    return <TaskBoardCreater toggle={this.state.modalOpen}/>;
   }
 
   renderWalls() {
@@ -193,24 +113,16 @@ class Tasks extends Component {
           
           {this.renderWalls()}
           
-          <div style={Object.assign({}, styles.wall, styles.createCard)} onClick={() => this.setState({modalOpen: true})}>
-            <AddIcon style={styles.addIcon}/>
-            <h2 style={styles.createCardTitle}>New Task Wall</h2>
-          </div>
+          {this.renderCreateModal()}
         </div>
 
-        {this.renderCreateModal()}
+        
       </PageContainer>
-    )
+    );
   }
 
   handleClick() {
-    const {dispatch} = this.props;
-    const name = this.refs.name;
-    dispatch(createTaskWall({name: name.value.trim()})).then(() => {
-      this.getWalls();
-      this.setState({modalOpen: false});
-    });
+    
   }
 }
 
