c933305095ac01f60479b26ed4a225741be4070c
fwchen <me@chenfangwei.com>
2016-10-07 14:44:20 +0800

    constants

---
+++ page/todo/constants.js
@@ -0,0 +1,10 @@
+export const repeatItems = [
+  {name: 'one day', value: 1},
+  {name: 'two day', value: 2},
+  {name: 'three day', value: 3},
+  {name: 'four day', value: 4},
+  {name: 'five day', value: 5},
+  {name: 'six day', value: 6},
+  {name: 'one week', value: 7},
+  {name: 'one month', value: 30}
+];
