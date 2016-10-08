075918ede43b9166e12e9206de244fb573076a82
fwchen <me@chenfangwei.com>
2016-10-09 00:53:07 +0800

    Button.js

--- components/widget/Button.js
+++ components/widget/Button.js
@@ -1,43 +1,21 @@
 import React, {Component} from 'react';
-import {ThemeConst} from '../../style/theme';
 
-const styles = {
-  button: {
-    borderRadius: '3px',
-    cursor: 'pointer'
-  },
-  small: {
-    
-  },
-  midde: {
-    
-  },
-  large: {
-    
-  },
-  default: {
-    border: `1px solid ${ThemeConst.border.color.default}`,
-    backgroundColor: '#fff'
-  },
-  primary: {
-    backgroundColor: ThemeConst.color.primary
-  },
-  disable: {
-    backgroundColor: ThemeConst.color.disable
-  }
-};
+import 'style/component/widget/button.scss';
 
 export class Button extends Component {
   static propTypes = {
     size: React.PropTypes.oneOf(['small', 'middle', 'large']),
     styleType: React.PropTypes.oneOf(['default', 'primary', 'disable'])
   };
+
+  buildClassName() {
+    const {size = 'middle', styleType = 'default'} = this.props;
+    return `button ${size} ${styleType}`;
+  }
   
   render() {
-    const {style, size = 'middle', styleType = 'default'} = this.props;
     return (
-      <button style={Object.assign(styles.button, style, styles[size], styles[styleType])}
-              onClick={this.props.onClick} type={this.props.type}>
+      <button className={this.buildClassName()} onClick={this.props.onClick} type={this.props.type}>
           {this.props.children}
       </button>
     );
