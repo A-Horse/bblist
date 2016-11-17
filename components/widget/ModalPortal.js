fc5f5a0463cba22f23cdfd8ff33aaf9a4a4bd3ca
fwchen <me@chenfangwei.com>
2016-11-17 18:58:39 +0800

    refactor ModalPortal

--- components/widget/ModalPortal.js
+++ components/widget/ModalPortal.js
@@ -28,152 +28,54 @@ export class ModalPortal extends Component {
     };
   }
 
-  componentDidMount() {
-    // Focus needs to be set when mounting and already open
-    if (this.props.toggle) {
-      this.setFocusAfterRender(true);
-      this.open();
-    }
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.closeTimer);
-  }
-
-  componentWillReceiveProps(newProps) {
-    // Focus only needs to be set once when the modal is being opened
-    if (!this.props.toggle && newProps.toggle) {
-      //this.setFocusAfterRender(true);
-      this.open();
-    } else if (this.props.toggle && !newProps.toggle) {
-      this.close();
-    }
-  }
-
-  componentDidUpdate() {
-    // if (this.focusAfterRender) {
-    //   //this.focusContent();
-    //   //this.setFocusAfterRender(false);
-    // }
-  }
-
-  setFocusAfterRender(focus) {
-    //this.focusAfterRender = focus;
-  }
-
-  open() {
-    if (this.state.afterOpen && this.state.beforeClose) {
-      clearTimeout(this.closeTimer);
-      this.setState({ beforeClose: false });
-    } else {
-      // focusManager.setupScopedFocus(this.node);
-      // focusManager.markForFocusLater();
-      this.setState({toggle: true}, function() {
-        this.setState({afterOpen: true});
-
-        if (this.props.toggle && this.props.onAfterOpen) {
-          this.props.onAfterOpen();
-        }
-      }.bind(this));
-    }
-  }
-
-  close() {
-    if (!this.ownerHandlesClose())
-      return;
-    if (this.props.closeTimeoutMS > 0)
-      this.closeWithTimeout();
-    else
-      this.closeWithoutTimeout();
-  }
-
   focusContent() {
-    //this.refs.content.focus();
-  }
-
-  closeWithTimeout() {
-    this.setState({beforeClose: true}, function() {
-      this.closeTimer = setTimeout(this.closeWithoutTimeout, this.props.closeTimeoutMS);
-    }.bind(this));
-  }
-
-  closeWithoutTimeout() {
-    this.setState({
-      beforeClose: false,
-      toggle: false,
-      afterOpen: false,
-    }, this.afterClose);
-  }
-
-  afterClose() {
-    // focusManager.returnFocus();
-    // focusManager.teardownScopedFocus();
+    this.refs.content.focus();
   }
 
   handleKeyDown(event) {
-    // if (event.keyCode == 9 /*tab*/) scopeTab(this.refs.content, event);
     if (event.keyCode == 27 /*esc*/) {
       event.preventDefault();
       this.requestClose(event);
     }
   }
 
-  handleOverlayClick(event) {
-    var node = event.target;
-
-    // while (node) {
-    //   if (node === this.refs.content) return
-    //   node = node.parentNode
-    // }
-
-    // if (this.props.shouldCloseOnOverlayClick) {
-    //   if (this.ownerHandlesClose())
-    //     this.requestClose(event);
-    //   else
-    //     this.focusContent();
-    // }
+  onOverlayClick(event) {
+    
   }
 
   requestClose(event) {
-    if (this.ownerHandlesClose())
-      this.props.onRequestClose(event);
+    this.props.close && this.props.close();
   }
 
-  ownerHandlesClose() {
-    return this.props.onRequestClose;
-  }
-
-  shouldBeClosed() {
-    return !this.props.toggle && !this.state.beforeClose;
-  }
 
   buildClassName(which, additional) {
     var className = CLASS_NAMES[which].base;
     if (this.state.afterOpen)
-      className += ' '+CLASS_NAMES[which].afterOpen;
+      className += ' '+ CLASS_NAMES[which].afterOpen;
     if (this.state.beforeClose)
-      className += ' '+CLASS_NAMES[which].beforeClose;
+      className += ' '+ CLASS_NAMES[which].beforeClose;
     return additional ? className + ' ' + additional : className;
   }
 
   render() {
-    return this.shouldBeClosed() ? div() : (
-      div({
-        ref: 'overlay',
-        className: this.buildClassName('overlay', this.props.overlayClassName),
-        onClick: this.props.onOverlayClick,
-        style: this.props.overlayStyle || {}
-      },
-          div({
-            ref: 'content',
-            className: this.buildClassName('content', this.props.className),
-            style: this.props.modalStyle || {},
-            tabIndex: "-1",
-            onKeyDown: this.handleKeyDown.bind(this)
-          },
-              this.props.children
-             )
-         )
-    );
+    // TODO
+    if (this.props.toggle) {
+      setTimeout(() => {
+        this.focusContent();
+      });
+    }
+
+    if (this.props.toggle) {
+      return (
+        <div ref='overlay' className={this.buildClassName('overlay', this.props.overlayClassName)}
+             onClick={this.onOverlayClick.bind(this)}>
+          <div ref='content' className={this.buildClassName('content', this.props.className)} tabIndex='-1'
+               onKeyDown={this.handleKeyDown.bind(this)}>
+            {this.props.children}
+          </div>
+        </div>
+      );
+    }
+    return React.DOM.noscript();
   }
 }
