1108a08f5e1f18f7ab1119031845d6ead5df4052
fwchen <me@chenfangwei.com>
2016-11-24 15:05:20 +0800

    comment action and reduec

---
+++ actions/task/task-card-comment.js
@@ -0,0 +1,42 @@
+import fetch from 'isomorphic-fetch';
+import {getAuthData} from 'utils/auth';
+import {handleHttpError} from '../../services/handle-error';
+import {createConfigWithAuth} from '../../utils/header';
+import {handleResponse, handleResponseWithoutJson} from '../../utils/http-handle';
+import {makeApiUrl} from '../../utils/api';
+
+export const TASKCARDCOMMENT_POST_REQUEST = 'TASKCARDCOMMENT_POST_REQUEST';
+export const TASKCARDCOMMENT_POST_SUCCESS = 'TASKCARDCOMMENT_POST_SUCCESS';
+export const TASKCARDCOMMENT_POST_FAILURE = 'TASKCARDCOMMENT_POST_FAILURE';
+
+function requestCreateTaskCardComment() {
+  return {
+    type: TASKCARDCOMMENT_POST_REQUEST
+  };
+}
+
+function createTaskCardCommentSucceess(comment) {
+  return {
+    type: TASKCARDCOMMENT_POST_SUCCESS,
+    playload: comment
+  };
+}
+
+function createTaskCardCommentError(error) {
+  return {
+    type: TASKCARDCOMMENT_POST_FAILURE,
+    playload: error,
+    error: true
+  };
+}
+
+export function createTaskCardComment(taskCardId, comment) {
+  const config = createConfigWithAuth('POST', comment);
+  return dispatch => {
+    dispatch(requestCreateTaskCardComment());
+    return fetch(makeApiUrl(`/task-card/${taskCardId}/comment`), config)
+      .then(handleResponse)
+      .then(() => dispatch(createTaskCardCommentSucceess()))
+      .catch(() => dispatch(createTaskCardCommentError()));
+  };
+}
