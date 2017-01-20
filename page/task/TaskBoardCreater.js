f8cce408a14c973d756336fe3435d5ed4af3af6f
fwchen <me@chenfangwei.com>
2017-01-20 11:50:17 +0800

    update

--- page/task/TaskBoardCreater.js
+++ page/task/TaskBoardCreater.js
@@ -5,57 +5,7 @@ import {CloseIcon, AddIcon} from 'services/svg-icons';
 import {Modal} from 'components/widget/Modal';
 import 'style/page/task/taskboard-creater.scss';
 
-
-const styles = {
-  wall: {
-    flex: '1 0 auto',
-    borderRadius: '3px',
-    margin: '8px 20px',
-    width: '200px',
-    height: '78px',
-    maxWidth: '200px',
-    cursor: 'pointer',
-    transition: 'box-shadow 218ms ease-in-out,transform 218ms ease-in-out,-webkit-transform 218ms ease-in-out',
-    boxShadow: '0 2px 3px 0 rgba(0,0,0,.0470588)',
-    // backgroundImage: 'url(/static/image/cover-internet.jpg)',
-    ':hover': {
-      transform: 'translate3d(0,-5px,0)',
-      boxShadow: '0 7px 21px rgba(0,0,0,.15)'
-    }
-  },
-  cardInfo: {
-    backgroundImage: 'linear-gradient(180deg,rgba(0,0,0,.3) 0, transparent)',
-    height: '50%'
-  },
-  cardTitle: {
-    
-  },
-  createCardTitle: {
-    
-  },
-  projectCard: {
-    
-  },
-  createCard: {
-    textAlign: 'center'
-  },
-  wallContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    marginTop: '1rem'
-  },
-  addIcon: {
-    height: '1.5rem',
-    width: '1.5rem',
-    borderRadius: '50%',
-    fill: 'white'
-  }
-};
-
-
-class TaskBoardCreater extends Component {
-  
+class TaskBoardCreater extends Component {  
   constructor() {
     super();
     this.state = {modalOpen: false};
@@ -77,12 +27,11 @@ class TaskBoardCreater extends Component {
   componentDidUpdate(props) {
   
   }
-  
-  
+
   renderModal() {
     return (
-      <Modal className='taskboard-creater' toggle={this.state.modalOpen}>
-        <div className='taskboard-creater'>
+      <Modal className='taskboard-creater-modal' toggle={this.state.modalOpen}>
+        <div>
           <button key='modal-close' className='close-button'>
             <CloseIcon className='clear-icon' onClick={() => this.setState({modalOpen: false})}/>
           </button>
@@ -99,9 +48,9 @@ class TaskBoardCreater extends Component {
 
   render() {
     return (
-      <div style={Object.assign({}, styles.wall, styles.createCard)} onClick={() => this.setState({modalOpen: true})}>
-        <AddIcon style={styles.addIcon}/>
-        <h2 style={styles.createCardTitle}>New Task Wall</h2>
+      <div className='taskboard-creater' onClick={() => this.setState({modalOpen: true})}>
+        <AddIcon/>
+        <h2>New Task Wall</h2>
         {this.renderModal()}
       </div>
     );
