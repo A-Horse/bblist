0fcef84481efe188f6fba5908afbd772931089b4
fwchen <me@chenfangwei.com>
2017-02-07 00:08:44 +0800

    update

--- page/task/Setting/Operation.js
+++ page/task/Setting/Operation.js
@@ -4,6 +4,7 @@ import {Modal} from 'components/widget/Modal';
 import ReactCrop from 'react-image-crop';
 import {ImageUploader} from 'components/ImageUploader';
 import {makeRemoteUrl} from 'services/remote-storage';
+import Button from 'components/widget/Button';
 
 import 'style/page/task/setting/infomation.scss';
 
@@ -13,6 +14,14 @@ class Operation extends Component {
     return (
       <div className='board-setting-operation'>
         <h3>Operation</h3>
+
+        <div className='board-operation-delete'>
+          <div className='board-operation-delete--heading'>Delete this Board</div>
+          <div>
+            <span>remove all data!</span>
+            <Button>Confirm delete</Button>
+          </div>
+        </div>
       </div>
     );
   }
