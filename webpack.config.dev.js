12ae0082244441b38bdd4e8456bc466551f7023f
fwchen <me@chenfangwei.com>
2017-07-03 11:22:26 +0800

    upgrade webpack to 3.0.0

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,25 +7,20 @@ var BellOnBundlerErrorPlugin = require('bell-on-bundler-error-plugin');
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.scss', '.css'],
-    modules: [
-      path.resolve('.'),
-      'node_modules'
-    ]
+    modules: [path.resolve('.'), 'node_modules']
   },
   devtool: 'source-map',
   devServer: {
     port: 9000,
-    proxy: { // NOTITE: // order is important
+    proxy: {
+      // NOTITE: // order is important
       '/api/t/': 'http://localhost:5502',
       '/api/ts/': 'http://localhost:5501',
       '/api': 'http://localhost:5500',
       '/storage': 'http://localhost:5500'
     }
   },
-  entry: [
-    'babel-polyfill',
-    './index'
-  ],
+  entry: ['babel-polyfill', './index'],
   output: {
     path: path.join(__dirname, 'dist/assets'),
     filename: 'bundle.js',
@@ -35,7 +30,7 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.BannerPlugin(fs.readFileSync('./.banner').toString()),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new BellOnBundlerErrorPlugin()
+    new webpack.optimize.ModuleConcatenationPlugin()
   ],
   module: {
     rules: [
@@ -53,16 +48,16 @@ module.exports = {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader"
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader"
+            loader: 'css-loader'
           },
           {
-            loader: "sass-loader"
+            loader: 'sass-loader'
           },
           {
-            loader: "autoprefixer-loader"
+            loader: 'autoprefixer-loader'
           }
         ],
         include: [path.resolve(__dirname, './style')]
@@ -71,18 +66,20 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader"
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader"
+            loader: 'css-loader'
           }
         ]
       },
       {
         test: /\.html$/,
-        use: [ {
-          loader: 'html-loader'
-        }]
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
       }
     ]
   }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,20 +4,14 @@ var webpack = require('webpack');
 
 const BellOnBundlerErrorPlugin = require('bell-on-bundler-error-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.scss', '.css'],
-    modules: [
-      path.resolve('.'),
-      "node_modules"
-    ]
+    modules: [path.resolve('.'), 'node_modules']
   },
-  entry: [
-    'babel-polyfill',
-    './index'
-  ],
+  entry: ['babel-polyfill', './index'],
   output: {
     path: path.join(__dirname, 'dist/assets'),
     filename: 'bundle.js',
@@ -27,11 +21,12 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.BannerPlugin(fs.readFileSync('./.banner').toString()),
     new BellOnBundlerErrorPlugin(),
-    new ExtractTextPlugin("styles.css"),
-    new UglifyJSPlugin({exclude: /\.css$/}),
+    new ExtractTextPlugin('styles.css'),
+    new UglifyJSPlugin({ exclude: /\.css$/ }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
   ],
   module: {
     rules: [
@@ -51,13 +46,13 @@ module.exports = {
           fallback: 'style-loader',
           use: [
             {
-              loader: "css-loader"
+              loader: 'css-loader'
             },
             {
-              loader: "sass-loader"
+              loader: 'sass-loader'
             },
             {
-              loader: "autoprefixer-loader"
+              loader: 'autoprefixer-loader'
             }
           ]
         }),
@@ -69,7 +64,7 @@ module.exports = {
           fallback: 'style-loader',
           use: [
             {
-              loader: "css-loader"
+              loader: 'css-loader'
             }
           ]
         })
