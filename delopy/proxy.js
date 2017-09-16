2ba3d6b5f3f9172a9592e3c67d6d9ed8fea375e4
fwchen <me@chenfangwei.com>
2017-09-16 22:13:17 +0800

    configure

--- delopy/proxy.js
+++ delopy/proxy.js
@@ -8,9 +8,11 @@ const buildPath = path.join(__dirname, '..', 'dist');
 const proxyOptions = require('./proxy-options');
 
 app.use(express.static(buildPath));
+
 Object.keys(proxyOptions).forEach(path => {
   app.use(path, proxy(proxyOptions[path]));
 });
+
 app.use((req, res) => res.sendFile(path.join(buildPath, 'index.html')));
 app.listen(9000);
 console.log('Built app serve at http://127.0.0.1:9000');
--- delopy/webpack.common.js
+++ delopy/webpack.common.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
