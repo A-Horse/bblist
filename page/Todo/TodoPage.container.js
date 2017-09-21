efeb7305e7fda9bc75cfa17b08023c50837b9c5e
fwchen <me@chenfangwei.com>
2017-09-21 16:08:29 +0800

    todo page

--- page/Todo/TodoPage.container.js
+++ page/Todo/TodoPage.container.js
@@ -4,8 +4,12 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import Actions, { makeActionRequestCollection } from '../../actions/actions';
 
-const mapStateToProps = (state, props) => {
-  return {};
+const mapStateToProps = state => {
+  return {
+    todoBoxs: state.todos
+      .get('todoBoxIds')
+      .map(id => state.todos.get('todoBoxEntities').get(String(id)))
+  };
 };
 
 const mapDispatchToProps = dispatch => {
