f5932e5018ef9b21b8ff4288836200368ba2675f
fwchen <me@chenfangwei.com>
2017-09-21 20:13:23 +0800

    hash to html color

--- page/Todo/TodoBoxs/TodoBoxs.js
+++ page/Todo/TodoBoxs/TodoBoxs.js
@@ -6,6 +6,14 @@ import TodoBoxCreater from '../TodoBoxCreater/TodoBoxCreater';
 
 import './TodoBoxs.scss';
 
+function djb2(str) {
+  var hash = 5381;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash << 5) + hash + str.charCodeAt(i);
+  }
+  return hash;
+}
+
 class TodoBoxs extends Component {
   static propTypes = {
     actions: PropTypes.object.isRequired,
@@ -16,6 +24,19 @@ class TodoBoxs extends Component {
 
   componentWillMount() {
     this.props.actions.GET_TODOBOXS_REQUEST();
+  }
+
+  buildIconColor(name) {
+    const hash = djb2(name);
+    const r = (hash & 0xff0000) >> 16;
+    const g = (hash & 0x00ff00) >> 8;
+    const b = hash & 0x0000ff;
+    return (
+      '#' +
+      ('0' + r.toString(16)).substr(-2) +
+      ('0' + g.toString(16)).substr(-2) +
+      ('0' + b.toString(16)).substr(-2)
+    );
   }
 
   render() {
@@ -38,6 +59,11 @@ class TodoBoxs extends Component {
                 return (
                   <div key={todoBox.get('id')}>
                     <Link to={`/todo/${todoBox.get('id')}`}>
+                      <i
+                        className="fa fa-archive"
+                        style={{ color: this.buildIconColor(todoBox.get('name')) }}
+                        aria-hidden="true"
+                      />
                       <span>{todoBox.get('name')}</span>
                     </Link>
                   </div>
