fa5780aa59d8e205b72576038619bb3c93ab38cb
fwchen <me@chenfangwei.com>
2017-09-21 16:08:02 +0800

    todo box test

--- page/Todo/TodoBoxs/TodoBoxs.test.js
+++ page/Todo/TodoBoxs/TodoBoxs.test.js
@@ -2,11 +2,18 @@ import React from 'react';
 import TodoBoxs from './TodoBoxs';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router';
+import { fromJS } from 'immutable';
 
 test('TodoBoxs', () => {
+  const actions = {
+    GET_TODOBOXS_REQUEST: jest.fn()
+  };
+
+  const todoBoxs = fromJS([{ name: 'Work Todo', id: 1 }, { name: 'Home Todo', id: 2 }]);
+
   const tree = renderer.create(
     <MemoryRouter>
-      <TodoBoxs />
+      <TodoBoxs actions={actions} todoBoxs={todoBoxs} />
     </MemoryRouter>
   );
   expect(tree).toMatchSnapshot();
