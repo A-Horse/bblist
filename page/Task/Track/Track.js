02679ca05d70264e8d710fd917b53f2a32528bd9
fwchen <me@chenfangwei.com>
2017-09-27 11:15:28 +0800

    great refactor

--- page/Task/Track/Track.js
+++ page/Task/Track/Track.js
@@ -6,6 +6,7 @@ import TaskCard from '../TaskCard/TaskCard';
 import TaskCardCreater from '../CardCreater/CardCreater';
 import { DropList } from 'components/widget/DropList/DropList';
 import ClickOutSide from 'components/utils/ClickOutSide';
+import { onTrackTopBarMouseDown } from './track-switch-helper';
 
 import { isEnterKey } from 'utils/keyboard';
 // import BoardCradDragHelper from 'services/board-card-drag-helper';
@@ -21,7 +22,8 @@ export class Track extends Component {
     track: PropTypes.object.isRequired,
     loginedUser: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired,
-    updateTrack: PropTypes.func.isRequired
+    updateTrack: PropTypes.func.isRequired,
+    addTaskCard: PropTypes.func.isRequired
   };
 
   state = {
@@ -61,107 +63,7 @@ export class Track extends Component {
   }
 
   onTopBarMouseDown(event) {
-    // TODO 只需要在content里query
-    const tracks = window.document.querySelectorAll('.task-track');
-    const thisTrack = this.domMain;
-    const movingTrack = thisTrack.cloneNode(true);
-
-    // const trackOffsetLeft = thisTrack.offsetLeft;
-
-    const pageContainer = window.document.body.querySelector('.board-container');
-
-    const trackHorMargin = 14;
-    const trackVerMargin = 15;
-
-    thisTrack.classList.add('shadowing');
-
-    const thisTrackRect = thisTrack.getBoundingClientRect();
-    const thisTrackLeft = thisTrackRect.left;
-    const thisTrackTop = thisTrackRect.top;
-    const trackOuterWidth = thisTrackRect.width + trackHorMargin * 2;
-
-    // TODO getMouseElementInnerOffset
-    const thisTrackMouseOffset = {
-      left: event.pageX - thisTrackLeft,
-      top: event.pageY - thisTrackTop
-    };
-
-    movingTrack.classList.add('moving');
-    movingTrack.style.height = thisTrack.offsetHeight + 'px';
-    movingTrack.style.width = thisTrack.offsetWidth + 'px';
-    movingTrack.style.top = thisTrackRect.top - trackVerMargin + 'px';
-
-    let currentTrackIndex = Number(thisTrack.dataset.index);
-
-    function onMouseMove(event) {
-      const movingOffset =
-        event.pageX + pageContainer.scrollLeft - thisTrackMouseOffset.left - trackHorMargin;
-      movingTrack.style.transform = `translate(${movingOffset}px, 0)`;
-
-      const mouseMovingOffset = event.pageX + pageContainer.scrollLeft;
-
-      const ii = Math.floor(mouseMovingOffset / trackOuterWidth);
-
-      if (ii === currentTrackIndex) {
-        return;
-      }
-
-      if (ii - currentTrackIndex === 1) {
-        tracks.forEach(track => {
-          const trackDataIndex = Number(track.dataset.index);
-          if (trackDataIndex === currentTrackIndex) {
-            const currentTransformLeft = Number(track.dataset.transformLeft) || 0;
-            track.dataset.transformLeft = currentTransformLeft + trackOuterWidth;
-            track.dataset.index = trackDataIndex + 1;
-            track.style.transform = `translate(${currentTransformLeft + trackOuterWidth}px, 0)`;
-          }
-          if (trackDataIndex === ii) {
-            const currentTransformLeft = Number(track.dataset.transformLeft) || 0;
-            track.dataset.transformLeft = currentTransformLeft - trackOuterWidth;
-            track.dataset.index = trackDataIndex - 1;
-            track.style.transform = `translate(${currentTransformLeft - trackOuterWidth}px, 0)`;
-          }
-        });
-        currentTrackIndex = ii;
-      } else if (currentTrackIndex - ii === 1) {
-        tracks.forEach(track => {
-          const trackDataIndex = Number(track.dataset.index);
-          if (trackDataIndex === currentTrackIndex) {
-            const currentTransformLeft = Number(track.dataset.transformLeft) || 0;
-            track.dataset.transformLeft = currentTransformLeft - trackOuterWidth;
-            track.dataset.index = trackDataIndex - 1;
-            track.style.transform = `translate(${currentTransformLeft - trackOuterWidth}px, 0)`;
-          }
-          if (trackDataIndex === ii) {
-            const currentTransformLeft = Number(track.dataset.transformLeft) || 0;
-            track.dataset.transformLeft = currentTransformLeft + trackOuterWidth;
-            track.dataset.index = trackDataIndex + 1;
-            track.style.transform = `translate(${currentTransformLeft + trackOuterWidth}px, 0)`;
-          }
-        });
-        currentTrackIndex = ii;
-      }
-    }
-
-    const self = this;
-    function onMouseUp() {
-      thisTrack.classList.remove('shadowing');
-      window.document.body.removeChild(movingTrack);
-      window.document.body.removeEventListener('mousemove', onMouseMove);
-      window.document.body.removeEventListener('mouseup', onMouseUp);
-
-      self.props.updateTaskTrackIndexs();
-    }
-
-    window.document.body.appendChild(movingTrack);
-
-    const movingOffset =
-      event.pageX + pageContainer.scrollLeft - thisTrackMouseOffset.left - trackHorMargin + 'px';
-    movingTrack.style.transform = `translate(${movingOffset}, 0)`;
-
-    window.document.body.addEventListener('mousemove', onMouseMove);
-    window.document.body.addEventListener('mouseup', onMouseUp);
-    // TODO body onblur
+    onTrackTopBarMouseDown(event, this);
   }
 
   resetDragMeta() {
