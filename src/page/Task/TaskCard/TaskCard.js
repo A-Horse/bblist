506e5dbf034d51fb0052c74ffd47c4691012c6fa
fwchen <me@chenfangwei.com>
2018-06-30 21:53:42 +0800

    update

--- src/page/Task/TaskCard/TaskCard.js
+++ src/page/Task/TaskCard/TaskCard.js
@@ -38,51 +38,6 @@ export class TaskCard extends Component<
     });
   };
 
-  onMouseDown = event => {
-    // NOTE 通过 element 判断 taskcard, 取index更新
-    return;
-    event.preventDefault();
-    event.stopPropagation();
-    const tracks = window.document.querySelectorAll('.task-track');
-
-    const thisCard = this.refs.main;
-
-    const movingCard = thisCard.cloneNode(true);
-    const pageContainer = window.document.body.querySelector('.board-track-container');
-
-    const mouseOffsetInCard = getMouseElementInnerOffset(thisCard, event);
-
-    const self = this;
-    function onMouseMove(event) {
-      if (!self.mouseMoving) {
-        // 防止点击的时候还是出现 clone
-        movingCard.style.height = thisCard.offsetHeight;
-        movingCard.style.width = thisCard.clientWidth;
-        movingCard.style.position = 'absolute';
-        movingCard.style.zIndex = '99999';
-        movingCard.style.left = event.pageX + 'px';
-        movingCard.style.top = event.pageY + 'px';
-
-        window.document.body.appendChild(movingCard);
-      }
-      const movingOffsetY = event.pageY;
-      const movingOffsetX = event.pageX + pageContainer.scrollLeft;
-      movingCard.style.left = '0';
-      movingCard.style.top = '0';
-      movingCard.style.transform = `translate(${movingOffsetX -
-        mouseOffsetInCard.left}px, ${movingOffsetY - mouseOffsetInCard.top}px)`;
-    }
-
-    function onMouseUp(event: Event) {
-      window.document.body.removeEventListener('mousemove', onMouseMove);
-      window.document.body.removeEventListener('mouseup', onMouseUp);
-      window.document.body.removeChild(movingCard);
-    }
-
-    window.document.body.addEventListener('mousemove', onMouseMove);
-    window.document.body.addEventListener('mouseup', onMouseUp);
-  };
-
   render() {
     const { card } = this.props;
     const { isDragging, connectDragSource, connectDragPreview } = this.props;
