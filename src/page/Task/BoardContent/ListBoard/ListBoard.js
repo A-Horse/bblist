4e6a0ab0ea8ec2b1ffdb6fa7cd6e4cb124ab6fa2
fwchen <me@chenfangwei.com>
2018-08-23 11:59:37 +0800

    update

--- src/page/Task/BoardContent/ListBoard/ListBoard.js
+++ src/page/Task/BoardContent/ListBoard/ListBoard.js
@@ -4,26 +4,29 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { makeActionRequestCollection } from '../../../../actions/actions';
+import { ListBoardCardContainer } from './ListBoardCard/ListBoardCard';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
+import { List } from 'immutable';
+
+import './ListBoard.less';
 
 @DragDropContext(HTML5Backend)
 export class ListBoard extends Component<
   {
     actions: any,
-    match: any,
-    history: any,
-    trackMap: any,
-    cardMap: any,
-    board: any,
-    loginedUser: any
+    cards: any
   },
   {}
 > {
   state = {};
 
   render() {
-    return <div className="board-track-container">column</div>;
+    return (
+      <div className="list-board-container">
+        {this.props.cards.map(card => <ListBoardCardContainer key={card.get('id')} card={card} />)}
+      </div>
+    );
   }
 }
 
@@ -34,10 +37,25 @@ const mapDispatchToProps = dispatch => {
 };
 
 const mapStateToProps = state => {
+  const trackMap = state.task2.get('trackMap');
+  const cardMap = state.task2.get('cardMap');
+  console.log(cardMap);
+  console.log(trackMap);
+  const tracks = trackMap.toArray();
+  console.log(tracks);
+  const cards = trackMap
+    .map(track => track.get('cards'))
+    .reduce((all, cards) => {
+      return all.concat(cards);
+    }, List())
+    .map(cardId => cardMap.get(cardId.toString()));
+  console.log('cards', cards);
+
   return {
     board: state.task2.get('board'),
-    trackMap: state.task2.get('trackMap'),
-    cardMap: state.task2.get('cardMap'),
+    trackMap,
+    cardMap,
+    cards,
     loginedUser: state.auth.get('loginedUser')
   };
 };
