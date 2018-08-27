05582d07297d59579dddc5584ee056ad890559be
fwchen <me@chenfangwei.com>
2018-08-27 22:19:27 +0800

    update

--- src/page/Task/BoardWall/BoardWall.js
+++ src/page/Task/BoardWall/BoardWall.js
@@ -20,29 +20,31 @@ export class BoardWall extends Component<{
   render() {
     return (
       <Content className="board-wall-page-content">
-        <TaskBoardCreater actions={this.props.actions} />
-        <div className="taskboard-boards">
-          <div className="board-group">
-            <div className="board-card-container">
-              {this.props.boardMap &&
-                this.props.boardMap.toArray().map(board => {
-                  return (
-                    <Link
-                      className="taskboard-card"
-                      style={{
-                        backgroundImage: board.get('cover')
-                          ? `url(${makeRemoteUrl(board.get('cover'))})`
-                          : `url(${DEFAULT_BOARD_COVER_SRC})`
-                      }}
-                      key={board.get('id')}
-                      to={`/task-board/${board.get('id')}`}
-                    >
-                      <div className="taskboard-card-info">
-                        <div className="taskboard-card-info--name">{board.get('name')}</div>
-                      </div>
-                    </Link>
-                  );
-                })}
+        <div className="inner-container">
+          <TaskBoardCreater actions={this.props.actions} />
+          <div className="taskboard-boards">
+            <div className="board-group">
+              <div className="board-card-container">
+                {this.props.boardMap &&
+                  this.props.boardMap.toArray().map(board => {
+                    return (
+                      <Link
+                        className="taskboard-card"
+                        style={{
+                          backgroundImage: board.get('cover')
+                            ? `url(${makeRemoteUrl(board.get('cover'))})`
+                            : `url(${DEFAULT_BOARD_COVER_SRC})`
+                        }}
+                        key={board.get('id')}
+                        to={`/task-board/${board.get('id')}`}
+                      >
+                        <div className="taskboard-card-info">
+                          <div className="taskboard-card-info--name">{board.get('name')}</div>
+                        </div>
+                      </Link>
+                    );
+                  })}
+              </div>
             </div>
           </div>
         </div>
