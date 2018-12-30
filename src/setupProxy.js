cbc82a4b22e0e0d9e8b25609707a7199dfeba87d
fwchen <me@chenfangwei.com>
2018-12-30 20:04:57 +0800

    add storage proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,4 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(proxy('/api', { target: 'http://localhost:5500/' }));
+  app.use(proxy('/storage', { target: 'http://localhost:5500/' }));
 };
