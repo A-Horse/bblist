83aaf498bf62aa77268e9cb7bc0c6e5dbf916284
fwchen <me@chenfangwei.com>
2020-12-04 23:03:33 +0800

    update

--- src/page/Schedule/ScheduleCalendar/ScheduleCalendar.tsx
+++ src/page/Schedule/ScheduleCalendar/ScheduleCalendar.tsx
@@ -109,33 +109,7 @@ export function ScheduleCalendar() {
         handleDragStart={handleDragStart}
       />
 
-      <Table
-        dataSource={[
-          {
-            name: 'Tony',
-            age: 13,
-            sex: '男',
-            grade: '六年级',
-          },
-          {
-            name: 'Hans',
-            age: 12,
-            sex: '男',
-            grade: '五年级',
-          },
-          {
-            name: 'Sally',
-            age: 10,
-            sex: '女',
-            grade: '三年级',
-          },
-        ]}
-      >
-        <Column prop="name" label="姓名"></Column>
-        <Column prop="age" label="年龄"></Column>
-        <Column prop="sex" label="性别"></Column>
-        <Column prop="grade" label="年级"></Column>
-      </Table>
+
     </div>
   );
 }
